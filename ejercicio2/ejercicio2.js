1ccaffa75e3aedac3a97858d2c41b66d145a5c48
Nathaly222 <146012550+Nathaly222@users.noreply.github.com>
2023-12-21 17:50:50 -0500

    video
    https://youtu.be/H0225P_fgvY

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -1,6 +1,8 @@
 'use strict'
 let arr = ["15", "50", "45"];
 arr.splice (1 , 0 ,'18');
-for ( let i = 0;  18 <=25 ; i++) {
-    alert(arr);
+for ( let i = 0; i < arr.length; i++) {
+   if ( arr[i] > 25 ) {
+     alert(arr[i]);
+   }
 }
--- prueba.js
+++ prueba.js
@@ -1,13 +1,23 @@
 'use strict'
-function students(name) {
+function students(name, correo, promedio) {
     this.name = name;
     this.correo= correo;
     this.promedio= promedio;
-}
-validar.function();{
-return this.promedio > 70 ; "Aprobado"; "Reprobado"
-}
-    
+    this.validarPromedio = function (){
+        if (this.promedio >= 70 ) { 
+            return "Aprobado";
+        } else{ 
+            return "Reprobado";
+        }
+    }
 
-let students = new students();
-alert(students);
+ this.obtenerUsuario = function () {
+     let email = this.correo;
+     let post = email.indexOf("@");
+     let usuario = email.substring(0,post);
+     return usuario;
+    };
+}
+let nathaly = new students("Nathaly", "caballero.nathaly@gmail.com", "80");
+alert(nathaly.validarPromedio());
+alert(nathaly.obtenerUsuario());
