275e774f050f71c803fca44dc048d2cc5993d5a7
Nathaly222 <146012550+Nathaly222@users.noreply.github.com>
2023-12-21 10:41:10 -0500

    Initial commit

---
+++ ejercicio2/ejercicio2.js
@@ -0,0 +1,6 @@
+'use strict'
+let arr = ["15", "50", "45"];
+arr.splice (1 , 0 ,'18');
+for ( let i = 0;  18 <=25 ; i++) {
+    alert(arr);
+}
---
+++ prueba.js
@@ -0,0 +1,13 @@
+'use strict'
+function students(name) {
+    this.name = name;
+    this.correo= correo;
+    this.promedio= promedio;
+}
+validar.function();{
+return this.promedio > 70 ; "Aprobado"; "Reprobado"
+}
+    
+
+let students = new students();
+alert(students);
